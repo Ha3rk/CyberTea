e1f247883002fdc0d408580fad3a37c049d7733a
ha3rk <greenpunctuations@gmail.com>
2025-01-06 03:13:57 +0200

    search added, forgot password done,

--- cybertea_project/settings.py
+++ cybertea_project/settings.py
@@ -201,6 +201,19 @@ LOGGING = {
 
 }
 
+# EMAIL DEVELOPEMENT CODE
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+'''
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'greenpunctuations@gmail.com'
+EMAIL_HOST_PASSWORD = 'guidemafyg wtzm xvwb rgmh ail1'''
+
+
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
